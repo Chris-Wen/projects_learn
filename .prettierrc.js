8385e2af1fd95cf748529ccc57da5d34b1c23e2a
Chris-Wen <whx555666@163.com>
2022-01-26 17:44:23 +0800

    基础配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  semi: false,
-  trailingComma: "all",
-  singleQuote: true,
-  jsxSingleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-};
+    semi: false, // 使用分号, 默认true
+    trailingComma: 'all',
+    singleQuote: true, // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+    jsxSingleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    arrowParens: 'avoid', // 单个参数的箭头函数不加括号 x => x
+}
