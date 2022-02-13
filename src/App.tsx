4b62e9420eb0f7ad9ba49bf1e6fa8c411db4a55b
Chris-Wen <whx555666@163.com>
2022-02-14 00:45:57 +0800

    react-router v6 路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React,  {Suspense } from "react";
-import routes from './routes'
-import { Skeleton } from 'antd'
-import { HashRouter, Routes, Route,  Navigate } from 'react-router-dom'
+import React from "react";
+import Router from './routes'
+import { HashRouter } from 'react-router-dom'
 import "./App.css";
 
-
-const App = () =>(
+const App = () => (
    <HashRouter>
-    <Suspense fallback={<Skeleton />}>
-      <Routes>
-        {routes.map((router) => (
-          <Route exact {...router} key={router.path} />
-        ))}
-        <Route path="/" element={<Navigate replace to="/appletsM/index" />} />
-      </Routes>
-    </Suspense>
+      <Router />
   </HashRouter>
 ) 
-
-  
-
 
 export default App;
