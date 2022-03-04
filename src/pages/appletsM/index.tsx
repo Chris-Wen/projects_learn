292e398c3ad72c769a482cab306057fd38face0a
Chris-Wen <whx555666@163.com>
2022-03-04 15:45:26 +0800

    1

--- src/pages/appletsM/index.tsx
+++ src/pages/appletsM/index.tsx
@@ -1,18 +1,15 @@
 import React, { FC, useState } from 'react'
 
-const HomePage:FC = () => {
+const HomePage: FC = () => {
     const [count, setState] = useState(0)
 
     return (
-      <>
-        Count: {count}
-        <button
-          onClick={() => setState(0)}>
-          Reset
-        </button>
-        <button onClick={() => setState(count-1)}>-</button>
-        <button onClick={() => setState(count+1)}>+</button>
-      </>
+        <>
+            Count: {count}
+            <button onClick={() => setState(0)}>Reset</button>
+            <button onClick={() => setState(count - 1)}>-</button>
+            <button onClick={() => setState(count + 1)}>+</button>
+        </>
     )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,32 @@
-
-import React, {Suspense, lazy } from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Skeleton } from 'antd'
 import { Navigate, useRoutes } from 'react-router-dom'
 
-const routes  = [
-  {
-    path: '/',
-    element: <Navigate replace to="/appletsM" />
-  },
-  {
-    path: '/appletsM',
-    component: lazy(() => import('../pages/appletsM')),
-    children: []
-  }
+const routes = [
+    {
+        path: '/',
+        element: <Navigate replace to='/appletsM' />,
+    },
+    {
+        path: '/appletsM',
+        component: lazy(() => import('../pages/appletsM')),
+        children: [],
+    },
 ]
- 
- 
+
 // 路由处理方式
-const generateRouter = (routers:any) => {
-  return routers.map((item:any) => {
-    item.children && (item.children = generateRouter(item.children))
-    item.component && (item.element = <Suspense fallback={<Skeleton />}><item.component /></Suspense>)
-    return item
-  })
+const generateRouter = (routers: any) => {
+    return routers.map((item: any) => {
+        item.children && (item.children = generateRouter(item.children))
+        item.component &&
+            (item.element = (
+                <Suspense fallback={<Skeleton />}>
+                    <item.component />
+                </Suspense>
+            ))
+        return item
+    })
 }
- 
+
 const Router = () => useRoutes(generateRouter(routes))
 export default Router
