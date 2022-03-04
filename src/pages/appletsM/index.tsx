c34685d723ab2d4efcd1839523ea6f8a4f02de84
Chris-Wen <whx555666@163.com>
2022-03-04 16:00:20 +0800

    husky

--- src/pages/appletsM/index.tsx
+++ src/pages/appletsM/index.tsx
@@ -1,11 +1,15 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 const HomePage: FC = () => {
-    const [count, setState] = useState(0)
+    const [count, setState] = useState<number>(0)
+
+    useEffect(() => {
+        console.log(count)
+    }, [])
 
     return (
         <>
-            Count: {count}
+            Count1: {count}
             <button onClick={() => setState(0)}>Reset</button>
             <button onClick={() => setState(count - 1)}>-</button>
             <button onClick={() => setState(count + 1)}>+</button>
